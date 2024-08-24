eabf0521b4a9c8383298cdb887037a190d45b9a3
AndreaVelazco <104018328+AndreaVelazco@users.noreply.github.com>
2024-08-24 14:31:11 -0500

    actualizacion del ejercicio 2
    Se agrego la descripción del ejercicio y la fecha. Tambien se agrego la condicional para verificar que sea correcta la funcion que se ingresa

--- Practica1/src/ejercicio2.kt
+++ Practica1/src/ejercicio2.kt
@@ -1,37 +1,37 @@
-//2. Piedra, papel, tijera
+//2-Piedra, papel, tijera. Realizar un juego sencillo de piedra papel o tijera, donde el usuario interactue con la computadora
 //Autor: Andrea del Rosario Velazco
 //Fecha de creacion: 22/08/2024
-//Ultima modificacion:
+//Ultima modificacion: 24/08/2024
 
 import kotlin.random.Random
 
 fun main() {
     val opciones = listOf("Piedra", "Papel", "Tijera")
 
-    // La computadora elige una opción aleatoria
+    // La opción aleatoria que dara la computadora
     val eleccionComputadora = opciones[Random.nextInt(opciones.size)]
 
     // Leer la elección del usuario
-    println("Elige una opción: Piedra, Papel o Tijera")
+    println("Elige una opción (Piedra, Papel o Tijera) : ")
     val eleccionUsuario = readLine()?.capitalize() ?: ""
 
-    // Verificar que la elección del usuario sea válida
+    // Verificar que sea válida la opcion del usuario
     if (eleccionUsuario !in opciones) {
         println("Opción inválida. Debes elegir entre Piedra, Papel o Tijera.")
         return
     }
 
-    // Mostrar las elecciones
-    println("Computadora eligió: $eleccionComputadora")
+    // Mostrar las elecciones de la computadora y del usuario
+    println("La computadora eligió: $eleccionComputadora")
     println("Tú elegiste: $eleccionUsuario")
 
     // Determinar el resultado
     val resultado = when {
         eleccionUsuario == eleccionComputadora -> "Empate"
-        eleccionUsuario == "Piedra" && eleccionComputadora == "Tijera" -> "Ganaste"
-        eleccionUsuario == "Papel" && eleccionComputadora == "Piedra" -> "Ganaste"
-        eleccionUsuario == "Tijera" && eleccionComputadora == "Papel" -> "Ganaste"
-        else -> "Perdiste"
+        eleccionUsuario == "Piedra" && eleccionComputadora == "Tijera" -> "¡Ganaste!"
+        eleccionUsuario == "Papel" && eleccionComputadora == "Piedra" -> "¡Ganaste!"
+        eleccionUsuario == "Tijera" && eleccionComputadora == "Papel" -> "¡Ganaste!"
+        else -> "Perdiste :("
     }
 
     // Mostrar el resultado
